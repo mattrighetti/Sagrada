1c130a014375ac5ec366253c50863a8dd880bc5c
Mattia Righetti <matt95.righetti@gmail.com>
2018-05-07 21:02:45 +0200

    Initial Round implementation

--- src/main/java/ingsw/model/Round.java
+++ src/main/java/ingsw/model/Round.java
@@ -1,11 +1,52 @@
 package ingsw.model;
 
-public class Round implements Runnable {
+import java.util.concurrent.atomic.AtomicBoolean;
 
+public class Round implements Runnable {
+    private Thread turno;
+    private Player player;
+    private AtomicBoolean hasMadeAMove;
+    private GameManager gameManager;
+/*
+    public Round(GameManager gameManager) {
+        this.gameManager = gameManager;
+        hasMadeAMove.set(false);
+    }
+
+    public void startRoundForPlayer(Player player) {
+        setPlayer(player);
+        turno.run();
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+*/
 
 
     @Override
     public void run() {
+        //executeMove();
+        //executeMove();
+        //gameManager.nextTurn();
+    }
+
+/*
+
+    private void executeMove() {
+        player.getUser().updateView();
+        while (!hasMadeAMove) {
+
+        }
+        hasMadeAMove.set(false);
+    }
+
+    public void makeMove() {
+        hasMadeAMove.set(gameManager);
+    }
+
+    public void doNotMakeAMove() {
 
     }
+    */
 }
