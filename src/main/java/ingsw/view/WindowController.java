258171a55a2db44005cc72283e54503028febb74
Mattia Righetti <mattia1.righetti@mail.polimi.it>
2018-06-16 00:51:55 +0200

    Improvements in Game PatternCard layout

--- src/main/java/ingsw/view/WindowController.java
+++ src/main/java/ingsw/view/WindowController.java
@@ -6,7 +6,6 @@ import ingsw.model.Dice;
 import ingsw.model.cards.patterncard.PatternCard;
 import ingsw.utilities.Tuple;
 import ingsw.view.nodes.DicePane;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
@@ -21,6 +20,9 @@ import java.util.Map;
 import java.util.ResourceBundle;
 
 public class WindowController implements Initializable {
+
+    @FXML
+    private AnchorPane anchorPane;
 
     @FXML
     private VBox windowFrameVBox;
@@ -39,7 +41,6 @@ public class WindowController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 5; j++) {
                 DicePane dicePane = new DicePane(i, j);
@@ -90,13 +91,8 @@ public class WindowController implements Initializable {
         this.networkType = networkType;
     }
 
-    @FXML
-    void onBreakWindowPressed(ActionEvent event) {
-        System.out.println("Broken window");
-    }
-
     void setGridPaneBackground(String patternCardName) {
-        patternCardGridPane.getStyleClass().add(patternCardName);
+        anchorPane.getStyleClass().add(patternCardName);
     }
 
     void setSelectedDice(Dice dice) {
