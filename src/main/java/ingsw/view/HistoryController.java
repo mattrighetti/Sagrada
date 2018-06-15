c3e55631b77d0ad6db7a5dc5c3237ca8970da3b0
Mattia Righetti <mattia1.righetti@mail.polimi.it>
2018-06-16 01:12:02 +0200

    Added back button to History GUI

--- src/main/java/ingsw/view/HistoryController.java
+++ src/main/java/ingsw/view/HistoryController.java
@@ -2,11 +2,14 @@ package ingsw.view;
 
 import ingsw.controller.network.NetworkType;
 import ingsw.utilities.MoveStatus;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -28,6 +31,9 @@ public class HistoryController implements SceneUpdater, Initializable {
 
     @FXML
     private TableColumn<MoveStatus, String> historyTableColumn;
+
+    @FXML
+    private Button backButton;
 
     private NetworkType networkType;
     private GUIUpdater application;
@@ -80,4 +86,7 @@ public class HistoryController implements SceneUpdater, Initializable {
     }
 
 
+    public void onBackButtonPressed(ActionEvent actionEvent) {
+        Platform.runLater(() -> application.launchSecondGUI(application.getUsername()));
+    }
 }
