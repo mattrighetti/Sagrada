e604e3dd566b77e6e9aa43932edb4ba9f2975ddb
Mattia Righetti <matt95.righetti@gmail.com>
2018-07-03 03:20:17 +0200

    Cleaned dead code

--- src/main/java/ingsw/utilities/GridCreator.java
+++ src/main/java/ingsw/utilities/GridCreator.java
@@ -1,17 +1,14 @@
 package ingsw.utilities;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import ingsw.model.Player;
 import ingsw.model.cards.patterncard.Box;
 import ingsw.model.cards.patterncard.PatternCard;
 
-import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -24,29 +21,6 @@ public final class GridCreator {
     }.getType();
 
     private GridCreator() {
-    }
-
-    /**
-     * Reads a pattern card's grid from JSON file passed as parameter
-     *
-     * @param path path of the JSON file of the PatternCard
-     * @return PatternCard grid
-     */
-    public static List<List<Box>> fromFile(GridJSONPath path) {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(path.getFilePath()));
-        try {
-            jsonReader = new JsonReader(bufferedReader);
-            if (jsonReader.hasNext()) {
-                Gson gson = new GsonBuilder().create();
-                return gson.fromJson(jsonReader, GRID_TYPE);
-            }
-        } catch (FileNotFoundException e) {
-            System.err.println("Couldn't find file");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return Collections.emptyList();
     }
 
     public static List<List<Box>> fromString(String jsonString) {
